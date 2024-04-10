8c0dca1d5a4dafff821f1f19e06fa4df1fc662d2
Jedidiah Foster <jedidiah.foster@gmail.com>
2024-04-10 10:01:42 -0700

    Changes to random resource, and resource names

--- locals.tf
+++ locals.tf
@@ -1,14 +1,17 @@
 # This file is for local values that may be required for the module to run.
 
 locals {
-  region         = data.aws_region.current.id
-  account_id     = data.aws_caller_identity.current.account_id
-  tags           = var.tags
-  parameter_name = var.parameter_name_override != null ? var.parameter_name_override : var.parameter_name == null ? format("/%s/%s", var.name, random_id.parameter-random.hex) : format("/%s/%s", var.name, var.parameter_name)
-  # Setting these 2 values to avoid mixing count and for_each. It also helps better name the outputs. The value is irrelavent. We only care about the key.
-  parameter_ignore = var.ignore_changes == true && var.parameter_map == null ? { (local.parameter_name) = "ignore" } : {}
-  parameter        = var.ignore_changes == false && var.parameter_map == null ? { (local.parameter_name) = "noignore" } : {}
+  account_id = data.aws_caller_identity.current.account_id
+  tags       = var.tags
+  # Setting these 3 values to avoid mixing count and for_each. It also helps better name the outputs. The value is irrelavent. We only care about the key.
+  parameter_ignore = var.ignore_changes == true && var.parameter_map == null ? { "parameter" = "ignore" } : {}
+  parameter        = var.ignore_changes == false && var.parameter_map == null ? { "parameter" = "noignore" } : {}
+  parameter_random = var.parameter_name_override == null && var.parameter_name == null ? { "random" = "random" } : {}
+
   # Setting these 2 values to provide a map with the full names for the parameter_map resources.
   parameter_ignore_map = var.ignore_changes == true && var.parameter_map != null ? merge({ for key, value in var.parameter_map : format("/%s/%s", var.name, key) => value }) : {}
   parameter_map        = var.ignore_changes == false && var.parameter_map != null ? merge({ for key, value in var.parameter_map : format("/%s/%s", var.name, key) => value }) : {}
+
+  # Setting the parameter name to be used in the resources.
+  parameter_name = var.parameter_name_override != null ? var.parameter_name_override : var.parameter_name != null ? format("/%s/%s", var.name, var.parameter_name) : format("/%s/%s", var.name, random_id.parameter-random["random"].hex)
 }
--- random.tf
+++ random.tf
@@ -1,4 +1,5 @@
 # Generates a 2 byte random number that we will append to the parameter name. This avoids naming conflicts.
 resource "random_id" "parameter-random" {
+  for_each    = local.parameter_random
   byte_length = 2
 }
